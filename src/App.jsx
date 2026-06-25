5a57fe6f7e3de9140efc45697ad5bbca5836f923
gade-6900 <gadepatil438@gmail.com>
2026-06-25 12:21:37 +0530

    started the developemt in form the scracth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import React,{useState} from "react";
+import { useState, useEffect } from "react";
+import SearchBox from "./Components/SerachBox";
+import EompoyeeCard from "./Components/EmpoyeeCard";
+import DepartmentFilter from "./Components/DepartmentFilter";
+import "./App.css";
 
 function App(){
+    const [employees, setEmployee] = useState("[]");
+    const [search, setSearch] = useState("");
+    const [department, setDepartment] = useState(" ");
+    useEffect(() => {
 
+        fetch("https://dummyjson.com/users")
+        .then((res) =>res.json())
+        .then((data) => setEmployee(data.users));
+    }, []);
+
+
+    
 }
-export default App;
