03f9c72b925aba17182029d65c6187afa8274fc6
gade-6900 <gadepatil438@gmail.com>
2026-06-25 12:28:59 +0530

    2 classes are write Serach and app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,76 @@
-import { useState, useEffect } from "react";
-import SearchBox from "./Components/SerachBox";
-import EompoyeeCard from "./Components/EmpoyeeCard";
-import DepartmentFilter from "./Components/DepartmentFilter";
+import { useEffect, useState } from "react";
+import SearchBar from "./components/SearchBar";
+import DepartmentFilter from "./components/DepartmentFilter";
+import EmployeeCard from "./components/EmployeeCard";
 import "./App.css";
 
-function App(){
-    const [employees, setEmployee] = useState("[]");
-    const [search, setSearch] = useState("");
-    const [department, setDepartment] = useState(" ");
-    useEffect(() => {
+function App() {
 
-        fetch("https://dummyjson.com/users")
-        .then((res) =>res.json())
-        .then((data) => setEmployee(data.users));
-    }, []);
+  const [employees, setEmployees] = useState([]);
+  const [search, setSearch] = useState("");
+  const [department, setDepartment] = useState("");
 
+  useEffect(() => {
 
-    
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => setEmployees(data.users));
+
+  }, []);
+
+  const filteredEmployees = employees.filter((emp) => {
+
+    const fullName =
+      `${emp.firstName} ${emp.lastName}`.toLowerCase();
+
+    const searchMatch =
+      fullName.includes(search.toLowerCase());
+
+    const departmentMatch =
+      department === ""
+        ? true
+        : emp.company.department === department;
+
+    return searchMatch && departmentMatch;
+
+  });
+
+  return (
+
+    <div className="container">
+
+      <h1>Employee Directory</h1>
+
+      <SearchBar
+        search={search}
+        setSearch={setSearch}
+      />
+
+      <DepartmentFilter
+        department={department}
+        setDepartment={setDepartment}
+        employees={employees}
+      />
+
+      {
+        filteredEmployees.length > 0 ?
+
+          filteredEmployees.map(emp => (
+
+            <EmployeeCard
+              key={emp.id}
+              employee={emp}
+            />
+
+          ))
+
+          :
+
+          <h2>No Employee Found</h2>
+      }
+
+    </div>
+  );
 }
+
+export default App;
---
+++ src/Components/SerachBox.jsx
@@ -0,0 +1,21 @@
+function SearchBar({ search, setSearch }) {
+
+    return (
+
+        <input
+
+            type="text"
+
+            placeholder="Search Employee"
+
+            value={search}
+
+            onChange={(e) => setSearch(e.target.value)}
+
+        />
+
+    );
+
+}
+
+export default SearchBar;
